7fe193c88af91fbdc4544a2b7ee4611a9e06980c
schen1001 <67364385+schen1001@users.noreply.github.com>
2024-01-16 14:32:20 +0100

    Update dashk_t04_17deployOnWeb.py
    Maybe there is a problem with dash_bootstrap_components

--- dashk_t04_17deployOnWeb.py
+++ dashk_t04_17deployOnWeb.py
@@ -38,10 +38,10 @@ print(df00.head(3))
 
 
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
-#app = dash.Dash(__name__, suppress_callback_exceptions=True)
+#app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
+server = app.server
 
 
 
